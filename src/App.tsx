3e2582b5d1c60e4f315fb98184ac227c8a6dd16a
mipopon <anchosanchos@gmail.com>
2023-07-29 23:09:25 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { FiChevronsDown } from 'react-icons/fi';
 
 import FullPageContainer from './components/FullPageContainer/FullpageContainer';
 
 const App = () => {
-  const myRef = useRef<HTMLInputElement>(null);
+  const nextSection = useRef<HTMLInputElement>(null);
+  const [scrollDown, setScrollDown] = useState(true);
 
   const executeScroll = () =>
-    myRef.current?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    nextSection.current?.scrollIntoView({ behavior: 'smooth', block: 'center' });
 
   return (
     <>
       <FullPageContainer count={0}>
         <h1 className='text-4xl'>Timeline Title</h1>
-        <button className='fixed bottom-12 text-4xl' onClick={executeScroll}>
+        <button
+          onScroll={() => {
+            setScrollDown(false);
+          }}
+          className={'fixed bottom-12 text-4xl' + (scrollDown ? '' : ' hidden')}
+          onClick={executeScroll}
+        >
           <FiChevronsDown />
         </button>
       </FullPageContainer>
       <FullPageContainer count={1}>
-        <h1 ref={myRef}>Next Screen</h1>
+        <h1 ref={nextSection}>Next Screen</h1>
       </FullPageContainer>
       <FullPageContainer count={2}>
         <h1>Next Next Screen</h1>
